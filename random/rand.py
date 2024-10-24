aa0a66f81c0acce50b5c647f72132c99130172dd
SamPM092 <sammich4294@gmail.com>
2024-10-24 18:04:28 +0700

    y

--- random/rand.py
+++ random/rand.py
@@ -4,7 +4,7 @@ import random
 import tkinter as tk
 from tkinter import ttk
 
-data_path = "D:\Anime"  // your path
+data_path = "D:\Anime"  #your path
 
 def button_clicked():
     folder_names = [name for name in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, name))]
@@ -16,13 +16,13 @@ def button_clicked():
 window = tk.Tk()
 window.title("Click the fckin button")
 
-button = tk.Button(window, text="Click Me", command=button_clicked, padx =20)
+button = tk.Button(window, text="Randomize", command=button_clicked, padx =20)
 button.pack()
 
 arial = ("Arial", 20, "bold italic")
 arial1 = ("Arial", 15)
 
-text = tk.Label(window, text="VVhat you should vvatch next :", pady=10, font=arial1)
+text = tk.Label(window, text="What you should vvatch next :", pady=10, font=arial1)
 text.pack()
 output_label = tk.Label(window, text="Output Text", pady=50, font=arial)
 output_label.pack()
