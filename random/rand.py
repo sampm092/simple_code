778caf7a27676b1b980f9b72e638bee2994e4739
SamPM092 <sammich4294@gmail.com>
2024-10-24 19:16:03 +0700

    now can use 'select file'

--- random/rand.py
+++ random/rand.py
@@ -3,10 +3,22 @@ import numpy as np
 import random
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog
 
-data_path = "D:\Anime"  #your path
+data_path = None  #your path
+
+def select_file():
+    # Open file dialog and store the selected file path
+    global data_path
+    data_path = filedialog.askdirectory()
+    if data_path:
+        label.config(text=f'Selected: {data_path}')
+    else:
+        label.config(text="No file selected.")
+
 
 def button_clicked():
+    global data_path
     folder_names = [name for name in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, name))]
     max_number = (len(folder_names)-1)
     random_number = random.randint(0, max_number)
@@ -14,10 +26,18 @@ def button_clicked():
     output_label.config(text= folder_names[random_number])
 
 window = tk.Tk()
-window.title("Click the fckin button")
+window.title("Title Randomizer")
 
-button = tk.Button(window, text="Randomize", command=button_clicked, padx =20)
-button.pack()
+# Create a button to trigger the file dialog
+button1 = tk.Button(window, text="Select File", command=select_file)
+button1.pack(pady=20)
+
+# Label to display the selected file path
+label = tk.Label(window, text="No file selected.")
+label.pack(pady=10)
+
+button2 = tk.Button(window, text="Randomize", command=button_clicked, padx =20)
+button2.pack()
 
 arial = ("Arial", 20, "bold italic")
 arial1 = ("Arial", 15)
@@ -27,7 +47,7 @@ text.pack()
 output_label = tk.Label(window, text="Output Text", pady=50, font=arial)
 output_label.pack()
 window_width = 400
-window_height = 200
+window_height = 400
 
 window.geometry(f"{window_width}x{window_height}")
 
