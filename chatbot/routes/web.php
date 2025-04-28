5acda66e78c522bdd01499aa257610991cc6bff1
SamPM092 <sammich4294@gmail.com>
2025-04-28 13:51:36 +0700

    it works but some input error

--- chatbot/routes/web.php
+++ chatbot/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::view('/chat', 'chat'); // Page with the chat UI
+Route::post('/chatbot', [ChatController::class, 'handle']); // API to handle messages
